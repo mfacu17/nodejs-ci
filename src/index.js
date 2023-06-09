87c3eac0712decfb50c1535d0b9152130c58563a
Facundo Montero <mfacu98@gmail.com>
2023-06-09 17:14:43 -0300

    working2

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const app = require('./app');
 const port = 3000 || process.env.PORT;
 
 app.listen(port, () => {
-    console.log(`The app is listening on ${port}`)
+    console.log(`The app is listening on ${port}.`)
 });
