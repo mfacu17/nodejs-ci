07218b6cc11d74bfffee70484d46d5ec2bb77182
Facundo Montero <mfacu98@gmail.com>
2023-06-09 17:07:50 -0300

    working

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (_req, res) => {
-    res.status(200).send('Hello world!')
+    res.status(200).send('Hello world')
 });
 
 module.exports = app;
